2be771fb3c3df9e45edc1e6175d6ab80efe37c39
Tatiana Medrán <avrutaty@gmail.com>
2024-11-10 21:52:02 -0300

    Actualizado y funcionando 10/11 21hs
    camina, salta, recolecta esfera y agregue square de enemigo

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,58 +4,53 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed;
-    public float jumpForce;
-    public Animator animator; // Referencia al Animator
+    public float speed = 1f; //es la misma speed que el componente del script playercontroller en player
+    public Animator animator;
 
-    private Rigidbody2D rb; // Referencia al Rigidbody2D
-    private bool isGrounded = true; // Para saber si el personaje está en el suelo
+    public float jumpForce = 4f; //rango de salto, en el inspector con 4 salta bien
+    public float rayCastLength = 0.1f;
+    public LayerMask floorLayer;
+
+    private bool onFloor;
+    private Rigidbody2D _rb; 
 
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-
-        if (animator == null)
-        {
-            Debug.LogError("Animator no asignado en el Inspector");
-        }
-        if (rb == null)
-        {
-            Debug.LogError("Rigidbody2D no encontrado en el GameObject");
-        }
+        _rb = GetComponent<Rigidbody2D>();
     }
 
+    //codigo para que camine
     void Update()
     {
-        // Movimiento horizontal
-        float horizontalMov = Input.GetAxis("Horizontal");
-        transform.Translate(new Vector3(horizontalMov * speed * Time.deltaTime, 0, 0));
+        float speedX = Input.GetAxis("Horizontal")*Time.deltaTime*speed;
+        animator.SetFloat("movement", speedX * speed);
 
-        // Actualizar la animación con el parámetro xVelocity
-        animator.SetFloat("xVelocity", Mathf.Abs(horizontalMov));
-
-        // Saltar si se presiona la tecla de espacio y el personaje está en el suelo
-        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
+        if (speedX < 0 )
         {
-            Jump();
+            transform.localScale = new Vector3(-1, 1, 1);
         }
+        if (speedX > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+
+        Vector3 posicion = transform.position;
+        transform.position = new Vector3(speedX + posicion.x, posicion.y, posicion.z);
+
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, rayCastLength, floorLayer);
+        onFloor = hit.collider != null;
+        if (onFloor && Input.GetKeyDown (KeyCode.Space))
+        {
+            _rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);  
+        }
+        animator.SetBool("onFloor", onFloor);
     }
 
-    void Jump()
+    void OnDrawGizmos() //figura imaginaria, la veo en el editor, no en el juego
     {
-        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-        animator.SetBool("isJumping", true);
-        isGrounded = false;
-    }
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + Vector3.down * rayCastLength);
 
-    // Método para detectar colisiones
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        // Verificar si la colisión es con el suelo
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            isGrounded = true;
-            animator.SetBool("isJumping", false); // Desactivar la animación de salto
-        }
     }
 }
